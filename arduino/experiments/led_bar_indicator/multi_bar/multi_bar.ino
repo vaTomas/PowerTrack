5b56831ffb4a9ed21decec030034068834804631
Vincent Alfred Tomas <135368023+vaTomas@users.noreply.github.com>
2023-11-04 00:47:03 +0800

    Modified socket switcher experiment

--- arduino/experiments/led_bar_indicator/multi_bar/multi_bar.ino
+++ arduino/experiments/led_bar_indicator/multi_bar/multi_bar.ino
@@ -1,35 +1,23 @@
 const uint8_t barLevelPin = 9;
 const uint8_t selectorPins[5] = {8, 7, 6, 5, 4};
-const uint8_t values[5] = {26, 51, 77, 110, 128};
+// const uint8_t values[5] = {26, 51, 77, 110, 128};
+const uint8_t values[5] = {128,26,51,110,77};
 
 void setup() {
   // Serial.begin(9600);
   pinMode(barLevelPin, OUTPUT);
   for (uint8_t pin : selectorPins) {
     pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
   }
 }
 
 void loop() {
   for (uint8_t selected = 0; selected < 5; selected++) {
-    delay(100);
-    for (uint8_t pin = 0; pin < 5; pin++) {
-        digitalWrite(selectorPins[pin], HIGH);
-    }
-
     analogWrite(barLevelPin, values[selected]);
-    // analogWrite(barLevelPin, 0);
-
-    for (uint8_t pin = 0; pin < 5; pin++) {
-
-      if (pin == selected) {
-        digitalWrite(selectorPins[pin], LOW);
-      } else {
-        digitalWrite(selectorPins[pin], HIGH);
-      }
-
-    }
-
-    
+    delay(8);
+    digitalWrite(selectorPins[selected], LOW);
+    delay(10);
+    digitalWrite(selectorPins[selected], HIGH);
   }
 }
--- arduino/experiments/socket_switcher/data_experiment/data_experiment.ino
+++ arduino/experiments/socket_switcher/data_experiment/data_experiment.ino
@@ -1,21 +1,20 @@
-const uint8_t selectorPins[5] = {4, 5, 6, 7, 8};
-const uint8_t pulseWritePin = 2;
-const uint8_t dataWritePin = 3;
+const uint8_t selectorPins[3] = {13, 12, 8};
+const uint8_t pulseWritePin = 3;
+const uint8_t dataWritePin = 2;
 
-bool data[5] = {0, 0, 0, 0, 0};
+
+bool data[8] = {true, true, true, true, true, true, true, true};
 
 void setup() {
   Serial.begin(9600);
 
-  digitalWrite(pulseWritePin, LOW);
-
-  for(int pin : selectorPins){
-    pinMode(pin, OUTPUT);
+  for(int selectorPin : selectorPins){
+    pinMode(selectorPin, OUTPUT);
   }
   pinMode(pulseWritePin, OUTPUT);
   pinMode(dataWritePin, OUTPUT);
 
-
+  digitalWrite(pulseWritePin, LOW);
   digitalWrite(dataWritePin, LOW);
 
   
@@ -25,28 +24,43 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     uint8_t selected = Serial.parseInt();
-    if(selected >= 5){  break; }
+    
+    if(selected >= 8){  
+      return; 
+    }
 
+    Serial.println("Selecting Pin..." + String(selected));
     selectPin(selected);
-
+    Serial.println("Pin Selected");
+    
+    Serial.println("Writing Data..." + String(data[selected]));
     data[selected] = !data[selected];
     digitalWrite(dataWritePin, data[selected]);
+    Serial.println("Data Written");
 
+    Serial.println("Saving Data..." + String(data[selected]));
     digitalWrite(pulseWritePin, HIGH);
+    delay(100);
     digitalWrite(pulseWritePin, LOW);
+    Serial.println("Data Saved");
+
+    for (uint8_t i = 0; i < 8; i++) {
+      Serial.print(data[i]);
+      Serial.print(" | ");
+    }
+    Serial.println();
 
     Serial.read();
   }
 }
 
 
-void selectPin(uint8_t number){
-  for(uint8_t i = 0; i < 5; i++){
-    if(i == number){
-      digitalWrite(selectorPins[i], LOW);
-    }
-    else{
-      digitalWrite(selectorPins[i], HIGH);
-    }
+void selectPin(uint8_t input) {
+  for (uint8_t i = 0; i < 3; i++) {
+    digitalWrite(selectorPins[i], input & 1);
+    input >>= 1;
   }
+  return;
 }
+
+
